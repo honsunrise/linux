d60127d49ea95ce4b0e0667404fae54b4247287d
Honsun Zhu <zhuhongxiang@zhuhongxiang.com>
2026-07-11 20:12:30 +0800

    bsp/drm + configs: fbdev v7.1 shims (CONFIG_FB=n, drm_client API rename)
    - sunxi_fbdev_platform.c: include <linux/vmalloc.h> (vmalloc/vmap
      no longer transitively provided); drm_client_framebuffer_create
      -> drm_client_buffer_create_dumb (v6.13 renamed and moved the
      4-arg wrapper); drm_client_framebuffer_delete -> drm_client_buffer_delete.
    - radxa_custom: disable CONFIG_FB + CONFIG_FRAMEBUFFER_CONSOLE.
      bsp/drivers/drm/Makefile switches to sunxi_drmfb_core.o (the DRM
      client-native path) when CONFIG_FB=n, avoiding sunxi_fbdev_core.c's
      FB_EVENT_BLANK / fb_notifier_call_chain dependencies which mainline
      removed. DRM console emulation stays via drm_fb_helper.

--- bsp/drivers/drm/sunxi_fbdev_platform.c
+++ bsp/drivers/drm/sunxi_fbdev_platform.c
@@ -10,6 +10,7 @@
  * warranty of any kind, whether express or implied.
  */
 #include <linux/version.h>
+#include <linux/vmalloc.h>
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(6, 1, 0)
 #include <drm/drm_fb_cma_helper.h>
 #include <drm/drm_gem_cma_helper.h>
@@ -244,7 +245,7 @@ int platform_fb_memory_alloc(struct fb_hw_info *hw_info, void **vir_addr, unsign
 #else
 	struct drm_gem_dma_object *gem;
 #endif
-	hw_info->buffer = drm_client_framebuffer_create(&hw_info->client, w, h, fb_fmt2_drm_fmt(fmt));
+	hw_info->buffer = drm_client_buffer_create_dumb(&hw_info->client, w, h, fb_fmt2_drm_fmt(fmt));
 	if (IS_ERR_OR_NULL(hw_info->buffer))
 		return PTR_ERR(hw_info->buffer);
 
@@ -326,7 +327,7 @@ int platform_fb_memory_free(struct fb_hw_info *info)
 
 	drm_client_buffer_vunmap(info->buffer);
 
-	drm_client_framebuffer_delete(info->buffer);
+	drm_client_buffer_delete(info->buffer);
 	info->buffer = NULL;
 
 	info->state.base.fb = NULL;
